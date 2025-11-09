f5e8697c1541aa5d0230beaa73ed2ae7e509f79b
Nasif <nasifabdullah396@gmail.com>
2025-11-10 02:40:04 +0800

    mod update booking by user part10

--- index.js
+++ index.js
@@ -712,34 +712,52 @@ app.put('/:person/:person_id/booking/:id', async (req, res) => {
                     return res.status(404).json({ error: 'Your booking not found' });
                 }
 
-                switch (true) {     // location, booking_date, booking_time, notes
-                    case location && booking_date && booking_time && notes: {
-                        res.status(400).json({ error: 'Access denied' });
-                        break;
-                    }
-                    case location: {    // location
-                        await client.query(`UPDATE bookings SET location = $1 WHERE id = $2 AND user_id = $3`, [location, id, person_id]);
-                        res.status(200).json({ message: 'Your booking successfully updated' });
-                        break;
-                    }
-                    case booking_date: {    // booking_date
-                        await client.query(`UPDATE bookings SET booking_date = $1 WHERE id = $2 AND user_id = $3`, [booking_date, id, person_id]);
-                        res.status(200).json({ message: 'Your booking successfully updated' });
-                        break;
-                    }
-                    case booking_time: {    // booking_time
-                        await client.query(`UPDATE bookings SET booking_time = $1 WHERE id = $2 AND user_id = $3`, [booking_time, id, person_id]);
-                        res.status(200).json({ message: 'Your booking successfully updated' });
-                        break;
-                    }
-                    case notes: {    // notes
-                        await client.query(`UPDATE bookings SET notes = $1 WHERE id = $2 AND user_id = $3`, [notes, id, person_id]);
-                        res.status(200).json({ message: 'Your booking successfully updated' });
-                        break;
-                    }
-                    default:
-                        res.status(400).json({ error: 'Invalid data' });
-                        // break;
+                // switch (true) {     // location, booking_date, booking_time, notes
+                //     case location && booking_date && booking_time && notes: {
+                //         res.status(400).json({ error: 'Access denied' });
+                //         break;
+                //     }
+                //     case location: {    // location
+                //         await client.query(`UPDATE bookings SET location = $1 WHERE id = $2 AND user_id = $3`, [location, id, person_id]);
+                //         res.status(200).json({ message: 'Your booking successfully updated' });
+                //         break;
+                //     }
+                //     case booking_date: {    // booking_date
+                //         await client.query(`UPDATE bookings SET booking_date = $1 WHERE id = $2 AND user_id = $3`, [booking_date, id, person_id]);
+                //         res.status(200).json({ message: 'Your booking successfully updated' });
+                //         break;
+                //     }
+                //     case booking_time: {    // booking_time
+                //         await client.query(`UPDATE bookings SET booking_time = $1 WHERE id = $2 AND user_id = $3`, [booking_time, id, person_id]);
+                //         res.status(200).json({ message: 'Your booking successfully updated' });
+                //         break;
+                //     }
+                //     case notes: {    // notes
+                //         await client.query(`UPDATE bookings SET notes = $1 WHERE id = $2 AND user_id = $3`, [notes, id, person_id]);
+                //         res.status(200).json({ message: 'Your booking successfully updated' });
+                //         break;
+                //     }
+                //     default:
+                //         res.status(400).json({ error: 'Invalid data' });
+                //         // break;
+                // }
+
+                if (location && booking_date && booking_time && notes) {
+                    res.status(400).json({ error: 'Access denied' });
+                } else if (location) {    // location
+                    await client.query(`UPDATE bookings SET location = $1 WHERE id = $2 AND user_id = $3`, [location, id, person_id]);
+                    res.status(200).json({ message: 'Your booking successfully updated' });
+                } else if (booking_date) {    // booking_date
+                    await client.query(`UPDATE bookings SET booking_date = $1 WHERE id = $2 AND user_id = $3`, [booking_date, id, person_id]);
+                    res.status(200).json({ message: 'Your booking successfully updated' });
+                } else if (booking_time) {    // booking_time
+                    await client.query(`UPDATE bookings SET booking_time = $1 WHERE id = $2 AND user_id = $3`, [booking_time, id, person_id]);
+                    res.status(200).json({ message: 'Your booking successfully updated' });
+                } else if (notes) {    // notes
+                    await client.query(`UPDATE bookings SET notes = $1 WHERE id = $2 AND user_id = $3`, [notes, id, person_id]);
+                    res.status(200).json({ message: 'Your booking successfully updated' });
+                } else {
+                    res.status(400).json({ error: 'Invalid data' });
                 }
 
                 break;
